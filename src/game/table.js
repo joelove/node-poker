de553252c255459a389b6e81eae55e0b858b42fd
Joe Love <joe.love@which.co.uk>
2014-06-17 22:56:57 +0100

    Added src folder

---
+++ src/game/table.js
@@ -0,0 +1,29 @@
+var core = require('../core');
+
+/**
+ * @namespace
+ */
+var POKER = POKER || {};
+
+(function(POKER) {
+  'use strict';
+
+  /**
+   * This class is responsible for handling the table status and main turn 
+   * logic. It also acts as the main notification handler.
+   *
+   * @class
+   */
+  POKER.Table = Base.extend({
+
+    /**
+     * @private
+     * @param {Array} adtechPlacements
+     */ 
+    _init: function(options) {
+      
+    }
+
+  });
+
+}(POKER));
